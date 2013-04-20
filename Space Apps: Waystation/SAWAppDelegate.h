57b01db1f7c57dbcd36ce9a27d649601e22881cf
Tim Capes <capestim@cs.toronto.edu>
2013-04-20 12:37:08 -0400

    Initial Commit

---
+++ Space Apps: Waystation/SAWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAWAppDelegate.h
+//  Space Apps: Waystation
+//
+//  Created by Tim Capes on 2013-04-20.
+//  Copyright (c) 2013 SpaceApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Space Apps: Waystation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Space Apps: Waystation
+//
+//  Created by Tim Capes on 2013-04-20.
+//  Copyright (c) 2013 SpaceApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAWAppDelegate class]));
+    }
+}
